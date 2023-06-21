0cdcabc94c912c95c80288fed042af51cf1ff4a6
Behrooz <b.raisdana@parshan.net>
2023-06-21 10:07:04 +0330

    working on tests

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # Steps of The Strategy
-1. Detect levels, their effectice times and their stength
+
+1. Detect levels, their effective times and their strength
 2. Detect Rally/Base/Drop periods
-a. sequence of 2 pairs of falling/rising peaks and valleys = Rally/Drop
-b. >3 candles with negative direction of highs and lows = Base
-c. >3 standard or samller candles >80% overlaps with previous one = Base
+
+- sequence of 2 pairs of falling/rising peaks and valleys = Rally/Drop
+- \>3 candles with negative direction of highs and lows = Base
+- \>3 standard or smaller candles >80% overlaps with previous one = Base
 
 4. Detect Pivots and create Levels
-a. after a confirmed trend reversal, expect to move 3 ATR towqrd trend + 1ATR inreverse
+   a. after a confirmed trend reversal, expect to move 3 ATR toward trend + 1ATR reverse
 6. Detect Gaps according to lower time data
 7. Detect Level reactions and Gap reactions
 8. Set stop loss and Target Profit and decide to trade or not.
 9. Put the order, Update SL according to price updates
-10. Do risk free and manage position according to 1st, 2nd, and 3rd TPs
+10. Do risk-free and manage position according to 1st, 2nd, and 3rd TPs
 11. of failed in 8 (>7) sequential trades shutdown for administrative inspection
--- test_LevelDetection.py
+++ test_LevelDetection.py
@@ -1,3 +1,5 @@
+import math
+
 from FigurePlotters import plot_ohlc_with_peaks_n_valleys
 import pandas as pd
 from LevelDetectionConfig import INFINITY_TIME_DELTA, config
@@ -29,7 +31,7 @@ def even_distribution(peaks: pd, valleys: pd):
 
 base_ohlc_ticks = pd.read_csv(f'{config.files_to_load[0]}.zip', index_col='date', parse_dates=['date'])
 if DEBUG: print(base_ohlc_ticks)
-base_peaks, _base_valleys = level_extractor(base_ohlc_ticks.tail(1000))
+base_peaks, base_valleys = level_extractor(base_ohlc_ticks.tail(1000))
 
 
 def test_time_switching():
@@ -49,13 +51,18 @@ def test_time_switching():
     _mapped_peaks_from_base = base_peaks['effective_time'].isin(config.times[i:])
 
     try:
-        assert _time_peaks == base_peaks[base_peaks['effective_time'].isin(config.times[i:])]
+        # todo: ValueError: Can only compare identically-labeled DataFrame objects
+        print("_time_peaks:")
+        print(_time_peaks.columns)
+        print("base_peaks[base_peaks['effective_time'].isin(config.times[i:])]:")
+        print(base_peaks[base_peaks['effective_time'].isin(config.times[i:])].columns)
+        assert _time_peaks.values == base_peaks[base_peaks['effective_time'].isin(config.times[i:])].values
     except AssertionError as e:
         plot_ohlc_with_peaks_n_valleys(ohlc=_time_ohlc_ticks, name=f'Test {config.times[i]}',
                                        peaks=_time_peaks, valleys=_time_valleys)
 
     try:
-        assert _time_valleys == _base_valleys[_base_valleys['effective_time'].isin(config.times[i:])]
+        assert _time_valleys == base_valleys[base_valleys['effective_time'].isin(config.times[i:])]
     except AssertionError as e:
         plot_ohlc_with_peaks_n_valleys(ohlc=_time_ohlc_ticks, name=f'Test {config.times[i]}',
                                        peaks=_time_peaks, valleys=_time_valleys)
@@ -65,35 +72,63 @@ def test_time_switching():
 
 def test_every_peak_is_found():
     for i in range(1, len(base_ohlc_ticks) - 1):
-        if base_ohlc_ticks.iloc[base_ohlc_ticks.index[i - 1]]['high'] < \
-                base_ohlc_ticks.iloc[base_ohlc_ticks.index[i]]['high'] > \
-                base_ohlc_ticks.iloc[base_ohlc_ticks.index[i + 1]]['high']:
-            assert base_peaks.iloc[base_ohlc_ticks.index[i]]['high'] == base_ohlc_ticks.iloc[i]['high']
+        if base_ohlc_ticks.iloc[i - 1]['high'] < \
+                base_ohlc_ticks.iloc[i]['high'] > \
+                base_ohlc_ticks.iloc[i + 1]['high']:
+            assert base_peaks[base_ohlc_ticks.index[i]]['high'] == base_ohlc_ticks.iloc[i]['high']
 
-def test_strenght_of_peaks():
-    for i in range(len(base_peaks)):
-        right_candles = base_ohlc_ticks.iloc[base_peaks.index[i] < base_peaks.index < (base_peaks.index[i] + base_peaks.iloc[i]['strength'])]
-        left_candles = base_ohlc_ticks.iloc[base_peaks.index[i] - base_peaks.iloc[i]['strength'] < base_peaks.index < (base_peaks.index[i] )]
-        assert base_peaks.iloc[i]['high'] > right_candles.max()['high']
-        if base_peaks.index[i] + base_peaks.iloc[i]['strength'] < base_peaks.index[-1]:
 
-            if base_peaks.index[i] - base_peaks.iloc[i]['strength'] > base_peaks.index[0]:
+def test_strength_of_peaks():
+    for _t_index in base_peaks.index.values:
+        right_candles = base_ohlc_ticks[_t_index < base_peaks.index <
+                                        (_t_index + base_peaks.iloc[_t_index]['strength'])]
+        assert base_peaks[_t_index]['high'] > right_candles.max()['high']
 
+        left_candles = base_ohlc_ticks[_t_index - base_peaks.iloc[_t_index]['strength']
+                                       < base_peaks.index < _t_index]
+        assert base_peaks[_t_index]['high'] > left_candles.max()['high']
+
+        if _t_index + base_peaks[_t_index]['strength'] < base_peaks.index[-1]:
+            if _t_index - base_peaks.iloc[_t_index]['strength'] > base_peaks.index[0]:
+                # we are the middle of series
+                assert base_peaks[_t_index + base_peaks['strength']]['high'] > base_peaks[_t_index]['high'] \
+                       or base_peaks[_t_index - base_peaks['strength']]['high'] > base_peaks[_t_index]['high']
             else:
-
+                # we are at the beginning of series
+                assert base_peaks[_t_index]['strength'] == _t_index - base_peaks.index[0]
         else:
+            # we are at the end of series
+            assert base_peaks[_t_index - base_peaks['strength']]['high'] > base_peaks[_t_index]['high']
 
-        try:
-        right_candle_at_strength = base_ohlc_ticks.iloc[base_peaks.index[i] + base_peaks.iloc[i]['strength']]
-        left_candle_at_strength = base_ohlc_ticks.iloc[base_peaks.index[i] - base_peaks.iloc[i]['strength']]
-        try:
+# todo: repeat above for valleys
 
-        assert base_peaks.iloc[i]['high'] <= right_candles.max()['high']
-
-
-def test_every_valley_is_found():
-    for i in range(1, len(base_ohlc_ticks) - 1):
-        if base_ohlc_ticks.iloc[base_ohlc_ticks.index[i - 1]]['low'] > \
-                base_ohlc_ticks.iloc[base_ohlc_ticks.index[i]]['low'] < \
-                base_ohlc_ticks.iloc[base_ohlc_ticks.index[i + 1]]['low']:
-            assert base_peaks.iloc[base_ohlc_ticks.index[i]]['low'] == base_ohlc_ticks.iloc[i]['low']
+# def test_strength_of_valleys():
+#     for _t_index in base_valleys.index.values:
+#         print(_t_index + base_valleys[_t_index]['strength'])
+#         right_candles = base_ohlc_ticks[_t_index < base_valleys.index <
+#                                         (_t_index + base_valleys[_t_index]['strength'])]
+#         assert base_valleys[_t_index]['low'] < right_candles.min()['low']
+#
+#         left_candles = base_ohlc_ticks[_t_index - base_valleys[_t_index]['strength']
+#                                        < base_valleys.index < _t_index]
+#         assert base_valleys[_t_index]['low'] < left_candles.min()['low']
+#
+#         if _t_index + base_valleys[_t_index]['strength'] < base_valleys.index[-1]:
+#             if _t_index - base_valleys[_t_index]['strength'] > base_valleys.index[0]:
+#                 # we are the middle of series
+#                 assert base_valleys[_t_index + base_valleys['strength']]['low'] < base_valleys[_t_index]['low'] \
+#                        or base_valleys[_t_index - base_valleys['strength']]['low'] < base_valleys[_t_index]['low']
+#             else:
+#                 # we are at the beginning of series
+#                 assert base_valleys[_t_index]['strength'] == _t_index - base_valleys.index[0]
+#         else:
+#             # we are at the end of series
+#             assert base_valleys[_t_index - base_valleys['strength']]['low'] < base_valleys[_t_index]['low']
+#
+#
+# def test_every_valley_is_found():
+#     for i in range(1, len(base_ohlc_ticks) - 1):
+#         if base_ohlc_ticks.iloc[i - 1]['low'] > \
+#                 base_ohlc_ticks.iloc[i]['low'] < \
+#                 base_ohlc_ticks.iloc[i + 1]['low']:
+#             assert base_peaks.iloc[i]['low'] == base_ohlc_ticks.iloc[i]['low']
