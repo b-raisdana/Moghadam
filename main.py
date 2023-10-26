de2fb39b38d68bdf7dd7a1ee9791697f8caadb00
Behrooz <b.raisdana@parshan.net>
2023-10-26 09:43:47 +0330

    check: Pivots at: 12-27 06:23:00 and 06:30:00 should be merged P: 4H/4:3@12-31|16:42=14187.57 not included in BULLISH: 20:00-12:00: M:1412.82ATR:841.72 No Pivot detected at: Dec 29 16:41 Dec 27 06:25

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from sys import exit
 
 import helper
 from BullBearSidePivot import generate_multi_timeframe_bull_bear_side_pivots
-from Candle import generate_ohlcv, generate_ohlcva, generate_multi_timeframe_ohlcva, read_multi_timeframe_ohlcv, \
-    read_multi_timeframe_ohlcva
+from Candle import generate_ohlc
 from PeakValleyPivots import generate_multi_timeframe_top_pivots
 from Config import config
 from MetaTrader import MT
@@ -13,15 +12,19 @@ from helper import date_range_to_string
 
 if __name__ == "__main__":
     config.under_process_date_range = date_range_to_string(days=60)
-    # custom_range_multi_timeframe_ohlcv = read_multi_timeframe_ohlcv('23-10-20.15-10T23-10-23.14-10')
-    # custom_range_multi_timeframe_ohlcv.loc[
-    #     custom_range_multi_timeframe_ohlcv.index.get_level_values(level='timeframe') == '1H']
-    read_multi_timeframe_ohlcva(config.under_process_date_range)
+
+    date_range_str = config.under_process_date_range
+    file_path: str = config.path_of_data
+    MT.extract_to_data_path(os.path.join(file_path, f'ohlc.{date_range_str}.zip'))
+    MT.load_rates()
+
+    # generatePeaks_n_Valleys
+    # _ohlc()
     # generate_multi_timeframe_peaks_n_valleys(config.under_process_date_range)
     # generate_multi_timeframe_candle_trend(config.under_process_date_range)
     # generate_multi_timeframe_bull_bear_side_trends(config.under_process_date_range, timeframe_shortlist=['4H'])
     # generate_multi_timeframe_bull_bear_side_pivots(config.under_process_date_range, timeframe_shortlist=['4H'])
+    generate_multi_timeframe_top_pivots(config.under_process_date_range)  # , timeframe_shortlist=['15min'])
 
-    # generate_multi_timeframe_top_pivots(config.under_process_date_range)  # , timeframe_shortlist=['15min'])
 
     exit()
